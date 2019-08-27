3dd405a3e8f239d4f96de77d43ef5939b79bdd37
gagmag212 <59160631@go.buu.ac.th>
2019-08-27 08:32:38 -0700

    END

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -14,6 +14,8 @@ import com.example.android.navigation.databinding.FragmentTitleBinding
 /**
  * A simple [Fragment] subclass.
  */
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 class TitleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
